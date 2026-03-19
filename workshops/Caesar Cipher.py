48d1a34f2fb9deecbcd95b9b6d75b9fbf368f1d8
Spades19 <120428155+Spades19@users.noreply.github.com>
2026-03-19 20:43:45 +0100

    W4

---
+++ workshops/Caesar Cipher.py
@@ -0,0 +1,37 @@
+def caesar(text, shift, encrypt=True):
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value'
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    if not encrypt:
+        shift = - shift
+# slicing and concatenation
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+# str.maketrans() used to map strings of equal length and return a table of corresponding characters
+    translation_table = str.maketrans(
+        alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
+
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+
+
+def decrypt(text, shift):
+    return caesar(text, shift, False)
+
+
+encrypted_text = ('Pbhentr vf sbhaq va hayvxryl cynprf.')
+decrypted_text = decrypt(encrypted_text, 13)
+print(decrypted_text)
+
+# def caesar():
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# shift = 5
+# shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+# translation_table = str.maketrans(alphabet, shifted_alphabet)
+# text = 'hello world'
+# encrypted_text = text.translate(translation_table)
+# print(encrypted_text)
