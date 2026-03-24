c0ffa7bdb52f89cf5475697dff0939bb8b97de2b
Spades19 <120428155+Spades19@users.noreply.github.com>
2026-03-24 23:15:05 +0100

    simple practice exercise on while loops

--- practice.py
+++ practice.py
@@ -1,8 +1,20 @@
-name = "Uriel"
-age = 21
+# name = "Uriel"
+# age = 21
 
-print(name, type(name))
-print(age, type(age))
+# print(name, type(name))
+# print(age, type(age))
 
 # reverse a string
-print(name[::-1])
+# print(name[::-1])
+
+secret_number = 3
+guess = 0
+
+while guess != secret_number:
+    guess = int(input('guess num from 1-5 :'))
+    if guess != secret_number:
+        print('wrong try again!')
+        if guess <= 0 or guess >= 5:
+            print('guess must be from 1-5')
+    else:
+        print('Yay you got it !!!')
